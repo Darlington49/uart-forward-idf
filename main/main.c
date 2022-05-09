6ec0c6480e9da9a3433d26626d3259c80f528160
Youcef Ali <youcef.ali@smartest.dz>
2022-05-09 10:24:16 +0200

    move includes to main.h

--- main/main.c
+++ main/main.c
@@ -1,41 +1,24 @@
-#include <stdio.h>
-#include "nvs_flash.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_log.h"
-#include "esp_system.h"
-#include "esp_event.h"
-
-// #include "telnet.h"
-
-#include <string.h>
-#include <sys/param.h>
-#include "esp_wifi.h"
-#include "esp_netif.h"
-#include "lwip/err.h"
-#include "lwip/sockets.h"
-#include "lwip/sys.h"
-
-#include "serial.h"
-#include "tcp.h"
-#include "connect.h"
-#include "mqtt.h"
+#include "main.h"
 
 void app_main(void)
 {
     esp_log_level_set("*", ESP_LOG_DEBUG);
     // esp_log_level_set(tag, ESP_LOG_INFO);
+    
     ESP_ERROR_CHECK(nvs_flash_init());
+
     // Uart Queue Creation;
     message_received_queue = xQueueCreate(10, sizeof(payload_ext_t));
+
     // Uart init
     uart_init();
+
     // Uart Task creation
     xTaskCreate(uart_event_task, "uart_event_task", 4096, NULL, 12, NULL);
-     wifi_init();
+    wifi_init();
 
     // Mqtt init
-     init_MQTT();
+    init_MQTT();
 
     ESP_ERROR_CHECK(wifi_connect_sta("AndroidAP9CB9", "00000000", 10000));
     xTaskCreate(tcp_server_task, "tcp_server", 4096, (void *)AF_INET, 5, NULL);
