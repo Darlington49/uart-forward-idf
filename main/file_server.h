48dca12571bea928018bb1514b8ba755808761ff
Youcef <youce.31@gmail.com>
2022-05-11 22:21:18 +0100

    Add File server logic

---
+++ main/file_server.h
@@ -0,0 +1,20 @@
+#ifndef file_server_H_ /* Include guard */
+#define file_server_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/param.h>
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include <dirent.h>
+
+#include "esp_err.h"
+#include "esp_log.h"
+
+#include "esp_vfs.h"
+#include "esp_spiffs.h"
+#include "esp_http_server.h"
+
+
+esp_err_t start_file_server(const char *base_path);
+#endif
